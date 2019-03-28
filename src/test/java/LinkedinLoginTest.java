314c63a9f2c95374cff0c9f2eabb28c78720e90b
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-03-28 21:17:10 +0200

    Lesson 4

---
+++ src/test/java/LinkedinLoginTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LinkedinLoginTest {
+
+    @Test
+    public void successfulLoginTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        WebElement emailField = driver.findElement(By.xpath("//input[@id='login-email']"));
+        WebElement passwordField = driver.findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = driver.findElement(By.xpath("//input[@id='login-submit']"));
+
+        emailField.sendKeys("linkedin.tst.yanina@gmail.com");
+        passwordField.sendKeys("Test123!");
+        signInButton.click();
+
+        WebElement profileNavigationItem = driver.findElement(By.xpath("//li[@id='profile-nav-item']"));
+
+    }
+}
