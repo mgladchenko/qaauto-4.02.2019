7891f88cbd6592aff76efb7893c198b9894c4c72
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2019-04-04 20:33:38 +0300

    Lesson 6(partial)

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -28,6 +28,36 @@ public class LinkedinLoginTest {
 
     @Test
     public void negativeWithEmptyValuesTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
 
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isPageLoaded(),
+                "Login page was not loaded.");
+
+        loginPage.login("", "");
+
+        Assert.assertTrue(loginPage.isPageLoaded(), "Login page is not loaded.");
+    }
+
+    @Test
+    public void negativeNavigatesToLoginSubmitTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        LoginPage loginPage = new LoginPage(driver);
+        Assert.assertTrue(loginPage.isPageLoaded(),
+                "Login page was not loaded.");
+
+        loginPage.login("linkedin.tst.yanina@gmail.com", "12345");
+
+        LoginSubmitPage loginSubmitPage = new LoginSubmitPage(driver);
+        Assert.assertTrue(loginSubmitPage.isPageLoaded(), "LoginSubmit page is not loaded.");
+
+        Assert.assertEquals(loginSubmitPage.getUserEmailValidationMessage(), "",
+                "userEmail validation message is incorrect.");
+        Assert.assertEquals(loginSubmitPage.getUserPasswordValidationMessage(),
+                "Hmm, that's not the right password. Please try again or request a new one.",
+                "userPassword validation message is incorrect.");
     }
 }
---
+++ src/test/java/LoginSubmitPage.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginSubmitPage {
+    WebDriver driver;
+
+    WebElement loginSubmitForm;
+
+    WebElement userEmailValidationMessage;
+    WebElement userPasswordValidationMessage;
+
+    public LoginSubmitPage(WebDriver driver) {
+        this.driver = driver;
+        initElements();
+    }
+
+    private void initElements() {
+        loginSubmitForm = driver.findElement(By.xpath("//form[@action='/checkpoint/lg/login-submit']"));
+        userEmailValidationMessage = driver.findElement(By.xpath("//div[@id='error-for-username']"));
+        userPasswordValidationMessage = driver.findElement(By.xpath("//div[@id='error-for-password']"));
+    }
+
+    public boolean isPageLoaded() {
+        return loginSubmitForm.isDisplayed();
+    }
+
+    public String getUserEmailValidationMessage() {
+        return userEmailValidationMessage.getText();
+    }
+
+    public String getUserPasswordValidationMessage() {
+        return userPasswordValidationMessage.getText();
+    }
+}
